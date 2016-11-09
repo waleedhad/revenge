b074202b9d4a32ec5e02b3f4fd6c995b26b65341
Waleed Al-Haddad <Waleed@Waleeds-MacBook-Pro.local>
2016-11-09 16:39:47 +0300

    Initial Commit

---
+++ revenge/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  revenge
+//
+//  Created by Waleed Al-Haddad on 9/11/16.
+//  Copyright © 2016 Waleed Al-Haddad. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
