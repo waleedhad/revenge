2494fc8610bc2fee1f294e78ebddae339808a840
Asim <Asim@asims-macbook-air.local>
2016-11-14 09:04:05 +0300

    hijhbpkbj

--- func.cpp
+++ func.cpp
@@ -4,7 +4,7 @@
 //
 //
 
-// first try in our github.
+// first try in our github.iuhiuhuohouhpou
 
 /*This source code copyrighted by Lazy Foo' Productions (2004-2015)
  and may not be redistributed without written permission.*/
